4d7ce3186c43031a507f0e398b603f1129da09eb
MostafaGadoo <73221261+MostafaGadoo@users.noreply.github.com>
2024-09-15 16:09:29 +0300

    Initial commit of portfolio website

---
+++ script.js
@@ -0,0 +1,13 @@
+script.jdocument.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
